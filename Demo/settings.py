890a4d62172aa3388b088422612ba58da930da54
vanshharode <vanshharode7522>
2024-10-28 12:38:48 +0530

    Edited Secrets in Settings and workflow also

--- Demo/settings.py
+++ Demo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q16*r=bafu()rmn%b+)1$j=xd*8_avy_7kg_@6xf_zbj3s%lb1'
+SECRET_KEY = os.environ['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
